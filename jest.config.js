1ffb9f9c2dd3ff5b7f0d70f9d23b12b0c244f4cf
Reynold Xin <rxin@databricks.com>
2020-09-05 15:37:00 -0700

    Add jest framework

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+module.exports = {
+  'roots': [
+    'src',
+  ],
+  'testMatch': [
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  'transform': {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
