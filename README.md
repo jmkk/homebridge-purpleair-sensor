0dcf191a0a0d4af45ebe763f1766b11f6a6093be
Jacek Suliga <jacek@suliga.net>
2023-12-30 11:05:03 -0800

    Major refactor of the plugin to dynamic platform. (#61)
    * Major refactor of the plugin to dynamic platform.
    
    Moved off the legacy plugin accessory.
    Added support for reporting humidity and temperature.
    
    Note: as is this plugin will require to be reconfigured from scratch,
    since the legacy plugin accessory configuration cannot be easily reused.
    
    Updated CI to use node 18 and 20.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 This is a Homebridge accessory plugin for monitoring air quality in Apple HomeKit. It creates virtual HomeKit
 air quality sensors based on real PurpleAir sensors. Once setup, you can also configure home automation
-based on air quality changes.
+based on air quality changes. It supports both using PurpleAir API (for sensors you do not own),
+and also connecting to your own sensors directly on your home network.
 
 This project was inspired by [SANdood's homebridge-purpleair](https://github.com/SANdood/homebridge-purpleair),
 but with a few changes:
@@ -14,15 +15,14 @@ but with a few changes:
 2. Support private sensors.
 3. Support VOC sensor reading.
 4. Support different averages, from realtime data, all the way to 1 hour average.
-5. When the sensor is opened by HomeKit, this plugin will dynamically try to refresh the sensor values. However,
-   it reuses the last updated value if it was fetched within 30 secs. This was done to prevent sending too many
-   API requests to PurpleAir's servers. If the sensor is not opened by HomeKit, the plugin will update its value
-   every 5 minutes by default.
+5. Sensor data gets refreshed every 5 minutes.
 6. Allow reporting AQI value instead of PM2.5 density in HomeKit. The author is more used to reading AQI value,
    but HomeKit has only a field for PM2.5 density value. The plugin allows you to configure displaying AQI value
    in the density field.
 7. Work with multiple sensors.
 8. Rewritten in TypeScript, with some unit tests.
+9. Optionally reports humidity and temperature in addition to air quality.
+10. Optionally supports local sensors on home network (no need for API read key).
 
 
 # Installation
@@ -43,51 +43,6 @@ sudo service homebridge restart  # replace this with the command you need to res
 
 You can configure this plugin using [homebridge-config-ui-x](https://www.npmjs.com/package/homebridge-config-ui-x) UI.
 
-If you want to configure manually by editing config.json file, add one entry per sensor you want to add to HomeKit
-to the `accessories` section. As an example:
-
-```json
-"accessories": [
-    {
-        "accessory": "PurpleAirSensor",
-        "sensor": "12345",
-        "name": "PurpleAir Indoor Sensor",
-        "conversion": "None",
-        "averages": "realtime"
-    },
-    {
-        "accessory": "PurpleAirSensor",
-        "sensor": "54321",
-        "name": "PurpleAir Outdoor Sensor",
-        "conversion": "None",
-        "averages": "10m"
-    }
-],
-```
-
-You can set the following fields:
-
-- `accessory` (required): must be `PurpleAirSensor`
-- `sensor` (required): the sensor number. Find the sensor number by going to <a href='https://www.purpleair.com/map'>PurpleAir's map</a> -> click on a sensor -> Look at the URL. It's the number right after 'select='.
-- `name` (required): name of the sensor.
-- `localIPAddress` (optional): Local IP address of the sensor, if using it directly on the local network
-- `apiReadKey` (optional): This is your API read key for using purpleair.com API - request one from contact@purpleair.com. It's not required when using local IP address above.
-- `key` (optional): API key for private sensors.
-- `conversion` (optional): Conversions help accomodate different types of pollution with different particle densities. Supports the following values:
-    - `None` (default)
-    - `AQandU`
-    - `LRAPA`
-    - `EPA`
-    - `WOODSMOKE`
-- `averages` (optional, default realtime): sensor reading averages, only used with `None` and `AQandU` conversions. Supports the following values:
-    - `realtime`
-    - `10m`: 10 Minute Average
-    - `30m`: 30 Minute Average
-    - `60m`: One Hour Average
-- `aqiInsteadOfDensity` (optional, default false): if true, use the PM2.5 density field to report AQI instead.
-- `updateIntervalSecs` (optional, default 300): number of seconds the plugin will wait before updating the sensor value again.
-- `verboseLogging` (optional, default false): if true, log all information at info level so you can see through homebridge logs what the plugin is doing without running homebridge in debug mode.
-
 
 # Development Notes
 
@@ -103,6 +58,7 @@ sudo npm publish
 
 
 # Changelog
+- 3.0.0: Major rewwrite to convert to the Platform plugin. Added humidity and temperatures reporting. Added ALT-CF3 conversion.
 - 2.1.0: Only fetch API fields needed for the core functionality of the plugin.
 - 2.0.2: Verbose network error logging.
 - 2.0.1: Refreshing dependencies, now requiring node >= 14.8.1
---
+++ src/platform.ts
@@ -0,0 +1,102 @@
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+
+import { PurpleAirPlatformAccessory } from './platformAccessory';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
+export class PurpleAirPlatform implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service = this.api.hap.Service;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: PlatformAccessory[] = [];
+
+  constructor(
+    public readonly log: Logger,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      this.discoverDevices();
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
+    this.accessories.push(accessory);
+  }
+
+  /**
+   * This is an example method showing how to register discovered accessories.
+   * Accessories must only be registered once, previously created accessories
+   * must not be registered again to prevent "duplicate UUID" errors.
+   */
+  discoverDevices() {
+    const registeredAccessories: string[] = [];
+
+    // Loop through the platform config and register each sensor as an accessory
+    const sensors = this.config.sensors || [];
+    for (const sensor of sensors) {
+      this.log.debug('Configuring sensor:', JSON.stringify(sensor));
+
+      const uuid = this.api.hap.uuid.generate(sensor.name);
+      registeredAccessories.push(uuid);
+
+      // see if an accessory with the same uuid has already been registered and restored from
+      // the cached devices we stored in the `configureAccessory` method above
+      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+
+      if (existingAccessory) {
+        // the accessory already exists
+        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+
+        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
+        existingAccessory.context.device = sensor;
+        this.api.updatePlatformAccessories([existingAccessory]);
+
+        // create the accessory handler for the restored accessory
+        new PurpleAirPlatformAccessory(this, existingAccessory);
+      } else {
+        // the accessory does not yet exist, so we need to create it
+        this.log.info('Adding new accessory:', sensor.name);
+
+        // create a new accessory
+        const accessory = new this.api.platformAccessory(sensor.name, uuid);
+
+        // store a copy of the device object in the `accessory.context`
+        // the `context` property can be used to store any data about the accessory you may need
+        accessory.context.sensor = sensor;
+
+        // create the accessory handler for the newly created accessory
+        new PurpleAirPlatformAccessory(this, accessory);
+
+        // link the accessory to your platform
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      }
+    }
+
+    // Remove any existing accessories that are no longer configured
+    const accessoriesToRemove = this.accessories.filter(accessory => !registeredAccessories.includes(accessory.UUID));
+    if (accessoriesToRemove.length > 0) {
+      this.log.info(`Unregistering ${accessoriesToRemove.length} accessories no longer in config file`);
+      this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, accessoriesToRemove);
+    }
+  }
+}
